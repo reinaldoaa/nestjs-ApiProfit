f293bcde39f2f711e39fecf3e29148847b727b5d
reinaldoaa <reinaldoa@hotmail.com>
2021-08-09 22:48:03 -0400

    synchronize false en la entidad categoria

--- src/modules/categorias/categorias.entity.ts
+++ src/modules/categorias/categorias.entity.ts
@@ -4,7 +4,7 @@ import {  Column, Entity,PrimaryColumn,BaseEntity, PrimaryGeneratedColumn, Index
 export class CategoriasEntity extends BaseEntity   {
     //primary key name constraint profit:cat_art_co_cat 
     @PrimaryColumn( {name:'co_cat', type:'char',unique:true,length:6,nullable:false}) coCat: string;
-    //name constraint: icat_des 
+    //name constraint PROFIT: icat_des 
     @Index('icat_des' )     
     @Column({name: 'cat_des',type:'varchar',length:60,nullable:false}) catDes: string;
     @Column({name:'co_us_in',type:'char',unique:false,length:6,nullable:false}) coUsIn: string;
